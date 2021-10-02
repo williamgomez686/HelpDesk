929605e9ad4e5c0a36df663ce72a482a7e4a32ab
William_Gomez_i7 <williamgomez686@hotmail.com>
2021-10-02 01:37:37 -0600

    Primer Commit remoto Agregando 2 cambios 1. creacion de nueva carpeta ViewModels 2. creacion de clase Estado

---
+++ HelpDesk/Models/Estado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelpDesk.Models
+{
+    public class Estado
+    {
+        public int Est_Id { get; set; }
+        public string Est_Descripcion { get; set; }
+        public DateTime Est_FchAlta { get; set; }
+        public string Est_UsuAlta { get; set; }
+        public string Est_Estado { get; set; }
+    }
+}
